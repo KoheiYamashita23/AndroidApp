243d776f7ecd7ddf786892e65412a3e6ccfb6abb
KoheiYamashita23 <221205179@ccmailg.meijo-u.ac.jp>
2023-09-05 15:42:58 +0900

    DataStoreの作成、save機能の追加

--- app/src/main/java/jp/ac/meijou/android/s221205179/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s221205179/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.TextView;
 
 import jp.ac.meijou.android.s221205179.databinding.ActivityMainBinding;
@@ -12,34 +13,35 @@ import jp.ac.meijou.android.s221205179.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        prefDataStore=PrefDataStore.getInstance(this);
 
-        binding.textButton.setOnClickListener(view->{
-            var text=binding.editTextText.getText().toString();
-            binding.text.setText(text);
+        prefDataStore.getString("name")
+                        .ifPresent(name->binding.text.setText(name));
+        binding.changeButton.setOnClickListener(view->{
+           var text=binding.editTextText.getText().toString();
+           binding.text.setText(text);
         });
 
-        binding.editTextText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                binding.text.setText(s.toString());
-            }
+        binding.saveButton.setOnClickListener(view ->{
+           var text=binding.editTextText.getText().toString();
+           prefDataStore.setString("name",text);
         });
 
+        Log.d("yamashita","onCreate text:"+binding.text.getText());
 
+
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        prefDataStore.getString("name")
+                .ifPresent(name->binding.text.setText(name));
+        Log.d("yamashita","onStart text:"+binding.text.getText());
     }
 }
